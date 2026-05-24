9f500c88024a5ee5369529fc2a3b2b2dfbfa1c21
CLR2001 <luro2501@student.miun.se>
2026-05-24 14:19:26 +0200

    small typo fix

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import User from '../models/user.model..js';
+import User from '../models/user.model.js';
 import authenticateToken from '../middleware/authenticate-token.js';
 import { getSimplifiedDetails } from '../utils/get-simplified-data.js';
 const router = express.Router();
